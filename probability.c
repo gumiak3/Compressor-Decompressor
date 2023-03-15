cf165a23debf1a06c0c6e02fdf5fe0865e23433a
Gumiak <gumiakk1234@gmail.com>
2023-03-15 16:46:31 +0100

    8,12,16 bits are working. They're stored in short variable

--- probability.c
+++ probability.c
@@ -1,8 +1,13 @@
 #include "probability.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
+int length = 64;
+
+void doubleLength(probability_t *array){
+    length*=2;
+    array = realloc(array,sizeof(*array) * length);
+}
 
 int contains(probability_t * array, short newBits){
     for(int i=0;i<11;i++){
@@ -14,7 +19,7 @@ int contains(probability_t * array, short newBits){
 }
 
 void resetArray(probability_t *array){
-    for(int i=0;i<127;i++)
+    for(int i=0;i<length;i++)
     {
         array[i].bits = 0;
         array[i].amount = 0;
@@ -30,27 +35,24 @@ probability_t * readBinaryFile(char *fileName, int flag){
     FILE *in = fopen(fileName,"rb");
     int bitsToRead = flag == 12 ? 2 : flag==16 ? 2 : 1;
     unsigned char buffer[2];
-    probability_t *probability = malloc(sizeof(*probability) * 127);
+    probability_t *probability = malloc(sizeof(*probability) * length);
     resetArray(probability);
     int index = 0;
-
     int mask = 0x00FF; // to extract useless 1
-    short previousBits = 0;
-    FILE *out = fopen("t.txt", "wb");
     if(in!=NULL){
         while(fread(buffer,sizeof(unsigned char),bitsToRead,in) != 0){
             short temp;
+            if((index+1) == length)
+                doubleLength(probability);
             if(flag==12) {
                 if(index==0)
                     probability[index].bits = buffer[0];
                 if(index%2==0){
                     temp = (probability[index].bits << 4) | (buffer[1] >> 4);
-//                    probability[index].bits = (probability[index].bits << 4) | (buffer[1] >> 4);
                     probability[index+1].bits = (buffer[1] << 4) & mask;
                     probability[index+1].amount = 1;
                 }else{
                     temp = (probability[index].bits << 4) | (buffer[0] & mask);
-//                    probability[index].bits = (probability[index].bits << 4) | (buffer[0] & mask);
                     probability[index+1].bits = (buffer[1] & mask);
                     probability[index+1].amount = 1;
                 }
@@ -72,7 +74,7 @@ probability_t * readBinaryFile(char *fileName, int flag){
     }
 
 
-    sortArray(probability);
+   // sortArray(probability);
     fclose(in);
     return probability;
 }
@@ -93,6 +95,8 @@ int getSize(probability_t *array){ // doesn't work on 12 bits yet
 void free_memory(probability_t *array){
     free(array);
 }
+
+
 // write to file in good direction
 //            unsigned char buff[2];
 //            buff[0] = (value >> 8) & 0xFF;
