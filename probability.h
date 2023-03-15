61c815eb632c2b69b1b6df4380cc1906d0c01b28
Gumiak <gumiakk1234@gmail.com>
2023-03-15 11:04:40 +0100

    working on reading bits

--- probability.h
+++ probability.h
@@ -1,11 +1,11 @@
 #ifndef COMPRESSOR_PROBABILITY_H
 #define COMPRESSOR_PROBABILITY_H
 typedef struct{
-    short word;
+    short bits;
     int amount;
 }probability_t;
 
-probability_t  * readBinaryFile(char *fileName, int flag);
-void sortArray(probability_t *array);
-
+probability_t  * readBinaryFile(char *, int );
+void sortArray(probability_t *);
+int getSize(probability_t *);
 #endif //COMPRESSOR_PROBABILITY_H
