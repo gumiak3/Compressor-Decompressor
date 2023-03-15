b6bfd3d45d4c5d88124abc23234514af7e524610
Pawel-Rusak <122686290+Pawel-Rusak@users.noreply.github.com>
2023-03-15 22:31:44 +0100

    still doesnt work, but has some improvements

--- tree.c
+++ tree.c
@@ -2,86 +2,84 @@
 #include <stdlib.h>
 #include <string.h>
 
-struct Node {
-    char symbol[9];
+typedef struct Input {
+    int symbol;
+    int frequency; 
+} Input;
+
+typedef struct Node {
+    int symbol;
     int frequency;
     struct Node* left;
     struct Node* right;
-};
+} Node;
 
-int cmp(const void* a, const void* b) {
-    const char* const *pa = a;
-    const char* const *pb = b;
-    return strcmp(*pa, *pb);
+void make_tree(struct Input *tab, int n, Node* root){
+    Node* nodes = malloc(sizeof(Node)*n);
+    for(int i = 0; i < n; i++){
+        nodes[i].symbol = tab[i].symbol;
+        nodes[i].frequency = tab[i].frequency;
+        nodes[i].left = NULL;
+        nodes[i].right = NULL;
+    }
+    while(n > 1){
+        Node* parent = malloc(sizeof(Node));
+        parent->frequency = nodes[0].frequency + nodes[1].frequency;
+        parent->left = &nodes[0];
+        parent->right = &nodes[1];
+        for(int i = 2; i < n; i++){
+            nodes[i-2] = nodes[i];
+        }
+        nodes[n-2] = *parent;
+        n--;
+        for(int i = 0; i < n-1; i++){
+            for(int j = 0; j < n-i-1; j++){
+                if(nodes[j].frequency > nodes[j+1].frequency){
+                    Node temp = nodes[j];
+                    nodes[j] = nodes[j+1];
+                    nodes[j+1] = temp;
+                }
+            }
+        }
+    }
+    root = &nodes[0];
+    //printf("%d", root->frequency);
 }
 
-int main() {
-    char s[1001];
-    scanf("%s", s);
-    int n = strlen(s)/8, k = 0;
-    char** v = (char**)malloc(n * sizeof(char*));
-    for(int i=0; i<n; i++){
-        v[i] = (char*)malloc(9 * sizeof(char));
-        for(int j=0; j<8; j++){
-            v[i][j] = s[i*8+j];
-        }
-        v[i][8] = '\0';
+void dfs(Node* root, char** codes, char* code, int depth){
+    if(root->left == NULL && root->right == NULL){
+        strcpy(codes[root->symbol], code);
+        printf("%s \n", codes[root->symbol]);
+        return;
     }
- 
-    qsort(v, n, sizeof(char*), cmp);
-    for(int i=0; i<=n - 1; i++)
-        if(strcmp(v[i], v[i+1]) != 0)
-            k++;
+    code[depth] = '0';
+    dfs(root->left, codes, code, depth+1);
+    code[depth] = '1';
+    dfs(root->right, codes, code, depth+1);
+}
+
+
+int main(void){
+    int n = 7;
+    struct Input *tab = malloc(sizeof(struct Input)*n);
+    for(int i = 0; i < n; i++){
+        tab[i].frequency = 2*i;
+        tab[i].symbol = i+1;
+    }
+    char** codes = malloc(sizeof(char*)*(1<<16));
+    for(int i = 0; i < (1<<16); i++){
+        codes[i] = malloc(sizeof(char)*16);
+    }
+    Node* root = malloc(sizeof(struct Node));
+    char* tmp = malloc(sizeof(char)*16);
+
+
+    make_tree(tab, n, root);
+    dfs(root, codes, tmp, 0);
+
+    printf("%s", codes[6]);
+    // codes[0] = "010101010";
     
-    char** w = (char**)malloc(k * sizeof(char*));
-    for(int i=0; i<k; i++){
-        w[i] = (char*)malloc(9 * sizeof(char));
-    }
-
-    int* f = (int*)malloc(k * sizeof(int));
-    w[0] = v[0];
-    f[0] = 1;
-    int j = 0;
-    for(int i=1; i<n; i++){
-        if(strcmp(v[i], v[i-1]) == 0){
-            f[j]++;
-        }
-        else{
-            j++;
-            w[j] = v[i];
-            f[j] = 1;
-        }
-    }
-    struct Node* tree = (struct Node*)malloc(k * sizeof(struct Node));
-    for(int i=0; i<k; i++){
-        strcpy(tree[i].symbol, w[i]);
-        tree[i].frequency = f[i];
-        tree[i].left = NULL;
-        tree[i].right = NULL;
-    }
-
-    for(int i=0; i<k; i++){
-        printf("%s %d\n", w[i], f[i]);
-    }
-
-    printf("%d\n", k);
-    for(int i=0; i<n; i++){
-        printf("%s ", v[i]);
-    }
-    
-    for(int i=0; i<n; i++){
-        free(v[i]);
-    }
-    free(v);
-
-    for(int i=0; i<k; i++){
-        free(w[i]);
-    }
-    free(w);
-
-    free(f);
-
-    free(tree);
-
-    return 0;
+    //printf("%s", codes[0]);
+    //printf("%d %d", tab[0].symbol, tab[0].frequency);
 }
