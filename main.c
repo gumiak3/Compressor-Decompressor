70d43617c3aaacc4b937137c88638435dbdf6275
Gumiak <gumiakk1234@gmail.com>
2023-03-16 23:47:12 +0100

    m

--- main.c
+++ main.c
@@ -4,8 +4,11 @@
 int main(int argc, char**argv) {
     probability_t *  probability = readBinaryFile(argv[1], atoi(argv[2]));
     for(int i=0;i<getSize(probability);i++){
-        if(probability[i].bits)
-            printf("%d : %d\n",probability[i].bits, probability[i].amount);
+        if(probability[i].bits){
+            printf("%d \n",probability[i].bits >> 4);
+            printf("%d \n",probability[i].bits );
+        }
+
     }
     printf("size: %d \n", getSize(probability)); // doesn't work on 12 bits yet!
     return 0;
