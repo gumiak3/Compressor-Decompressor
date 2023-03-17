da449951f065cc72ad02ff414bbd355ca925ce6f
Gumiak <gumiakk1234@gmail.com>
2023-03-17 16:24:36 +0100

    rest of files

--- main.c
+++ main.c
@@ -2,16 +2,18 @@
 #include <stdlib.h>
 #include "dataReader.h"
 #include "splitData.h"
+#include "frequency.h"
 int main(int argc, char**argv) {
     int size = 0;
     char *data = readData(argv[1],&size);
     char rest;
-    short *splittedData = splitData(data,&size,16,&rest);
+    short *splittedData = splitData(data,&size,8,&rest);
+    printf("size %d\n", size);
+    frequency_t * freqArray = getFrequency(splittedData,&size);
     printf("size %d\n", size);
     for(int i=0;i<size;i++){
-        printf("%c, %c", splittedData[i] >> 8, splittedData[i]);
+        printf("%c -> %d\n",freqArray[i].bits, freqArray[i].frequency);
     }
-    printf(",%c", rest);
     return 0;
 
 }
