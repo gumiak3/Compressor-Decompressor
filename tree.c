44e734e0d93e3b3fa5994ca32f4870d180e08645
Pawel-Rusak <122686290+Pawel-Rusak@users.noreply.github.com>
2023-03-18 22:14:40 +0100

    fixed creating too many nodes
    to do:
    - creting codes related to bits sequences
    - now nodes array is size of 2*n, but some of them arent being used, so be cautious of that

--- tree.c
+++ tree.c
@@ -16,82 +16,85 @@ typedef struct Node {
     bool bylo;
 } Node;
 
-void make_tree(struct Input *tab, int n, Node* root){
-    Node* nodes = malloc(sizeof(Node)*n);
-    for(int i = 0; i < n; i++){
-        nodes[i].symbol = tab[i].symbol;
-        nodes[i].frequency = tab[i].frequency;
-        nodes[i].left = NULL;
-        nodes[i].right = NULL;
-    }
+void make_tree(struct Input *tab, int n){
+	Node* nodes = malloc(sizeof(Node)*(n*2));
+	for(int i = 0; i < n; i++){
+		nodes[i].symbol = tab[i].symbol;
+		nodes[i].frequency = tab[i].frequency;
+	}
+	for(int i = 0; i < n*2; i++){
+		nodes[i].bylo = false;
+		nodes[i].left = NULL;
+		nodes[i].right = NULL;
+	}
+	int k = 0;
+	for(int i = 0; i < n; i++){
+		if(nodes[i].bylo == false){
+			k++;
+		}
+	}
+	printf("%d \n", k);
+	int mini1, mini2;
+	while(k>1){
+		mini1 = -1;
+		mini2 = -1;
+	        for(int i = 0; i < n; i++){
+        	    if(nodes[i].bylo == false){
+                	if(mini1 == -1)
+                    		mini1 = i;
+                	else if(mini2 == -1)
+                    		mini2 = i;
+                	else if(nodes[i].frequency < nodes[mini1].frequency || nodes[i].frequency < nodes[mini2].frequency){
+                    		if(nodes[mini1].frequency < nodes[mini2].frequency){
+                        		mini2=i;
+                    		}
+                   		 else{
+                        		mini1=i;
+                	    	}
+        	        }
+	            }
+        	}		
+		printf("%d %d\n", mini1, mini2);
+	
+		nodes[n].frequency = nodes[mini1].frequency + nodes[mini2].frequency;
+		nodes[n].left = &nodes[mini1];
+		nodes[n].right = &nodes[mini2];
+		nodes[mini1].bylo = true;
+		nodes[mini2].bylo = true;
+		n++;
+		k = 0;
+		for(int i = 0; i < n; i++){
+			if(nodes[i].bylo == false){
+				k++;
+			}
+		}
+	}
 
-    for(int i = 0; i < 2*n; i++){
-        nodes[i].bylo = false;
-    }
+	Node* new_nodes = realloc(nodes, sizeof(Node) * n);
+	nodes = new_nodes;
 
-    int k = 2, mini1, mini2;
-
-    while(k>1){
-        k=0;
-        for(int i = 0; i < n; i++){
-            if(nodes[i].bylo == false){
-                k++;
-            }
-        }
-        printf("diho raz ");
-        printf("%d ", k);
-        mini1 = -1;
-        mini2 = -1;
-        for(int i = 0; i < n; i++){
-            if(nodes[i].bylo == false){
-                if(mini1 == -1)
-                    mini1 = i;
-                else if(mini2 == -1)
-                    mini2 = i;
-                else if(nodes[i].frequency < nodes[mini1].frequency || nodes[i].frequency < nodes[mini2].frequency){
-                    if(nodes[mini1].frequency < nodes[mini2].frequency){
-                        mini2=i;
-                    }
-                    else{
-                        mini1=i;
-                    }
-                }
-                
-            }
-        }
-        printf("%d %d ", mini1, mini2);
-
-        nodes[n].frequency = nodes[mini1].frequency + nodes[mini2].frequency;
-        nodes[n].left = &nodes[mini1];
-        nodes[n].right = &nodes[mini2];
-        printf("%d %d %d \n", nodes[n].left->symbol, nodes[n].right->symbol, nodes[n].frequency);
-        nodes[mini1].bylo = true;
-        nodes[mini2].bylo = true;
-        n++;
-    }
-
-    // if(nodes)
-    // printf("%d", parent->left->symbol);
-    // nodes[n-2] = *parent;
-    // n--;
-
-    // for(int i = 0; i < n-1; i++){
-    //     for(int j = 0; j < n-i-1; j++){
-    //         if(nodes[j].frequency > nodes[j+1].frequency){
-    //             Node temp = nodes[j];
-    //             nodes[j] = nodes[j+1];
-    //             nodes[j+1] = temp;
-    //         }
-    //     }
-    // }
-
-
-    // for(int i = 0; i < n; i++){
-    //     printf("%d %d \n", nodes[i].symbol, nodes[i].frequency);
-    // }
-    // root = &nodes[0];
-    // printf("%d \n", root->frequency);
+	for(int i = 0; i < n; i++){
+		printf("Node at index %d: \n", i);
+		printf("Symbol:%d ", nodes[i].symbol);
+		printf("frequency:%d ", nodes[i].frequency);
+		if(nodes[i].left != NULL){
+		    printf("Left child symbol: %d ", nodes[i].left->symbol);
+		}
+		else{
+		    printf("Left child is NULL ");
+		}
+		if(nodes[i].right != NULL){
+		    printf("Right child symbol: %d ", nodes[i].right->symbol);
+		}
+		else{
+		    printf("Right child is NULL ");
+		}
+		printf("Bylo:%d ", nodes[i].bylo);
+		printf("\n\n");
+	}
+	printf("Array length: %ld\n", sizeof(nodes) / sizeof(nodes[0]));
 }
+
 
 int main(void){
     int n;
@@ -109,12 +112,6 @@ int main(void){
     tab[4].frequency = 4;
     tab[5].symbol = 1110;
     tab[5].frequency = 18;
-    struct Node* leafs = malloc(sizeof(Node)*n*2);
+	make_tree(tab, n);
 
-    // for(int i = 0; i < n; i++){
-    //     printf("%d %d \n", leafs[i].symbol, leafs[i].frequency);
-    // }
-    struct Node* root = malloc(sizeof(Node));
-    make_tree(tab, n, root);
-    // printf("%hd", root->right->symbol);
-}
+};
