50f3e3b09a09f7d6f9e4bf1b8dd805bf503229ad
Gumiak <gumiakk1234@gmail.com>
2023-03-20 21:58:38 +0100

    test

--- compress.c
+++ compress.c
@@ -1,6 +1,7 @@
 #include "compress.h"
 #include <stdio.h>
 #include <string.h>
+#include "frequency.h"
 char convertToBits(char *code, int length){
     char output = 0x0;
     for(int i=0;i<length;i++){
@@ -8,7 +9,6 @@ char convertToBits(char *code, int length){
             output |= (1 << (length- 1 - i));
         }
     }
-
     return output;
 }
 
@@ -21,9 +21,10 @@ char * getCode(short bits, codes_t *codes, int size, int *codeLength){
     }
     return NULL;
 }
+//
+//void addControlSums(char *rest,)
 
-
-void compressFile(short *splittedData,int dataSize, codes_t *codes, int codesSize){
+void compressFile(short *splittedData,int dataSize, codes_t *codes, int codesSize, char *rest){
     FILE *out = fopen("output.txt", "wb");
     char *headline = "#LP#\n";
     fwrite(headline,sizeof(*headline),strlen(headline),out);
@@ -34,12 +35,12 @@ void compressFile(short *splittedData,int dataSize, codes_t *codes, int codesSiz
     int i=0;
     char *code = getCode(splittedData[i++],codes,codesSize,&codeLength);
     while(i <= dataSize){
-        while(bufforIndex < 7  && codeIndex < codeLength){
+        while(bufforIndex < 8  && codeIndex < codeLength){
             buffor[bufforIndex++] = code[codeIndex++];
         }
-        if(bufforIndex == 7){
+        if(bufforIndex == 8){
             // wpisujemy do pliku
-            char toWrite = convertToBits(buffor, 7);
+            char toWrite = convertToBits(buffor, 8);
             fwrite(&toWrite,sizeof(char),1,out);
             bufforIndex = 0;
         }
@@ -51,12 +52,13 @@ void compressFile(short *splittedData,int dataSize, codes_t *codes, int codesSiz
     }
     if(bufforIndex!=0){
         // zostala jakas reszta nie zapisana
-        for(int i=6;i>=bufforIndex;i--){
+        for(int i=7;i>=bufforIndex;i--){
             buffor[i]='0';
         }
-        char toWrite = convertToBits(buffor, 7);
+        char toWrite = convertToBits(buffor, 8);
         fwrite(&toWrite,sizeof(char),1,out);
         // dodac jakas sume kontrolna, ktora powie ze na koncu jest jakas reszta
     }
-
+    // add to the end the rest which we had at the beginning
+    fclose(out);
 }
