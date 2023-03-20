6a7a3c2b307b4e8ed0566b058162489af918a229
Gumiak <gumiakk1234@gmail.com>
2023-03-20 23:48:46 +0100

    compress module, still need to consider any extra bits that will be left on the end of file

--- compress.c
+++ compress.c
@@ -1,7 +1,7 @@
 #include "compress.h"
 #include <stdio.h>
 #include <string.h>
-#include "frequency.h"
+
 char convertToBits(char *code, int length){
     char output = 0x0;
     for(int i=0;i<length;i++){
@@ -25,7 +25,7 @@ char * getCode(short bits, codes_t *codes, int size, int *codeLength){
 //void addControlSums(char *rest,)
 
 void compressFile(short *splittedData,int dataSize, codes_t *codes, int codesSize, char *rest){
-    FILE *out = fopen("output.txt", "wb");
+    FILE *out = fopen("output.bin", "wb");
     char *headline = "#LP#\n";
     fwrite(headline,sizeof(*headline),strlen(headline),out);
     int codeLength=0;
@@ -52,13 +52,14 @@ void compressFile(short *splittedData,int dataSize, codes_t *codes, int codesSiz
     }
     if(bufforIndex!=0){
         // zostala jakas reszta nie zapisana
-        for(int i=7;i>=bufforIndex;i--){
-            buffor[i]='0';
+        for(int j=7;j>=bufforIndex;j--){
+            buffor[j]='0';
         }
         char toWrite = convertToBits(buffor, 8);
         fwrite(&toWrite,sizeof(char),1,out);
         // dodac jakas sume kontrolna, ktora powie ze na koncu jest jakas reszta
     }
-    // add to the end the rest which we had at the beginning
+    if(*rest != 0)
+        fwrite(rest,sizeof(char),1,out);
     fclose(out);
 }
--- main.c
+++ main.c
@@ -1,23 +1,25 @@
-
+#include <stdio.h>
 #include "dataReader.h"
 #include "splitData.h"
 #include "frequency.h"
-#include "tree.h"
+#include "compress.h"
+#include <stdlib.h>
 int main(int argc, char**argv) {
     int size = 0;
     char *data = readData(argv[1],&size);
     char rest;
-    short *splittedData = splitData(data,&size,12,&rest);
+    short *splittedData = splitData(data,&size,8,&rest);
+
     int dataSize = size;
     frequency_t * freqArray = getFrequency(splittedData,&size);
-    int a = test(freqArray);
-    //codes_t * codes = malloc(sizeof(*codes)*size);
+
+//    codes_t * codes = malloc(sizeof(*codes)*size);
 //    for(int i=0;i<size;i++){
 //        codes[i].bits = freqArray[i].bits;
 //    }
-  //  codes[0].code = "0001";
-    //codes[1].code = "1000";
-   // compressFile(splittedData,dataSize,codes,size,&rest);
+//    codes[0].code = "0001";
+//    codes[1].code = "1000";
+//    compressFile(splittedData,dataSize,codes,size,&rest);
 
     return 0;
 
